80b43135ac0c41d3f035ead7ec36b5eda467b39f
Aashi20997 <parikhaashi97.AP@gmail.com>
2023-08-16 02:41:06 +0530

    Initial Commit

---
+++ TableViewExample/ItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCell.swift
+//  TableViewExample
+//
+//  Created by Aashi Parikh on 16/08/23.
+//
+
+import UIKit
+class ItemCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+}
